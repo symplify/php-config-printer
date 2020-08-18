6b5641bb5c74cdc26c3a5b16121228f6b13d9198
TomasVotruba <tomas.vot@gmail.com>
2020-08-19 00:02:19 +0200

    [PhpConfigPrinter] Decopule EmptyLineNodeDecorator

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpParser\NodeFinder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Yaml\Parser;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Migrify\PhpConfigPrinter\\', __DIR__ . '/../src');
+    $services->set(NodeFinder::class);
+    $services->set(Parser::class);
+};
