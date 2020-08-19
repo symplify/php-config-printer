1b1fd910a74664dc878152cd467827db34d23814
TomasVotruba <tomas.vot@gmail.com>
2020-08-19 16:37:06 +0200

    add empty config support

--- tests/Printer/SmartPhpConfigPrinter/Fixture/expected_file.php
+++ tests/Printer/SmartPhpConfigPrinter/Fixture/expected_file.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Migrify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\FirstClass;
+use Migrify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source\SecondClass;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
-    $services->set('SomeClass')
+    $services->set(FirstClass::class)
         ->call('configure', [['some_key' => 'some_value']]);
+
+    $services->set(SecondClass::class);
 };
