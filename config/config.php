d6f393b386ed083f76d940d17db7c88ac818bbfb
Tomas Votruba <tomas.vot@gmail.com>
2020-09-23 16:39:10 +0200

    [PhpConfigPrinter] Add constant support + inline value object support (#181)
    * [PHPConfigPrinter] Improve README
    
    * [PHPConfigPrinter] Add support for constant conversion
    
    * static fixes
    
    * decouple ServiceConfigurationDecorator
    
    * add inline value object configurable support
    
    * fix copy of directory

--- config/config.php
+++ config/config.php
@@ -2,12 +2,17 @@
 
 declare(strict_types=1);
 
+use Migrify\PhpConfigPrinter\ValueObject\Option;
 use PhpParser\BuilderFactory;
 use PhpParser\NodeFinder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\Yaml\Parser;
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::INLINE_VALUE_OBJECT_FUNC_CALL_NAME, 'Migrify\SymfonyPhpConfig\inline_value_object');
+
     $services = $containerConfigurator->services();
 
     $services->defaults()
@@ -21,4 +26,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(NodeFinder::class);
     $services->set(Parser::class);
     $services->set(BuilderFactory::class);
+
+    $services->set(ParameterProvider::class);
 };
