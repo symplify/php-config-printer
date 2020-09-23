b898610db2816a803bf6026b7da61b534934e364
Tomas Votruba <tomas.vot@gmail.com>
2020-09-23 16:39:10 +0200

    [PhpConfigPrinter] Add constant support + inline value object support (#181)
    * [PHPConfigPrinter] Improve README
    
    * [PHPConfigPrinter] Add support for constant conversion
    
    * static fixes
    
    * decouple ServiceConfigurationDecorator
    
    * add inline value object configurable support
    
    * fix copy of directory

---
+++ src/ValueObject/Option.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\PhpConfigPrinter\ValueObject;
+
+final class Option
+{
+    /**
+     * @api
+     * @var string
+     */
+    public const INLINE_VALUE_OBJECT_FUNC_CALL_NAME = 'inline_value_object_func_call_name';
+}
