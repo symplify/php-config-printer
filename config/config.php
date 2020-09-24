5c9f4be66dc03ed77c8f365b0447fb574ea40893
TomasVotruba <tomas.vot@gmail.com>
2020-09-24 16:00:41 +0200

    add missing docntsanst

--- config/config.php
+++ config/config.php
@@ -13,6 +13,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::INLINE_VALUE_OBJECT_FUNC_CALL_NAME, 'Migrify\SymfonyPhpConfig\inline_value_object');
 
+    $parameters->set(Option::INLINE_VALUE_OBJECTS_FUNC_CALL_NAME, 'Migrify\SymfonyPhpConfig\inline_value_objects');
+
     $services = $containerConfigurator->services();
 
     $services->defaults()
