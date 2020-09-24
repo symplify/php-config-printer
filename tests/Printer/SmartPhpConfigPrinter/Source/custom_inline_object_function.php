2a1ba2fd384d26ba58a48e1a26ba90bdb6ecd2a4
Tomas Votruba <tomas.vot@gmail.com>
2020-09-24 15:32:00 +0200

    [PhpConfigPrinter] Add multi value objects function support (#182)

--- tests/Printer/SmartPhpConfigPrinter/Source/custom_inline_object_function.php
+++ tests/Printer/SmartPhpConfigPrinter/Source/custom_inline_object_function.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Migrify\PhpConfigPrinter\Tests\Printer\SmartPhpConfigPrinter\Source;
 
-function custom_inline_function(): void
+function custom_inline_object_function(): void
 {
 }
