77dc996aaa77af6c112d1a34b4c097cc833267c0
Tomas Votruba <tomas.vot@gmail.com>
2023-02-22 19:06:53 +0000

    update PHPUnit to 10

--- rector.php
+++ rector.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
+        PHPUnitSetList::PHPUNIT_100,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         LevelSetList::UP_TO_PHP_81,
@@ -30,5 +32,10 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         '*/Source/*',
         '*/Fixture/*',
+        // keep prefix
+        StringClassNameToClassConstantRector::class => [
+            // keep the container class string, to avoid prefixing
+            __DIR__ . '/src/NodeFactory/ContainerConfiguratorReturnClosureFactory.php',
+        ],
     ]);
 };
